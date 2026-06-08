f93800749756ac0407d67d9e026ee3f4bfc4119c
Sarthak Mukherjee <sarthakmukherjee@virtualemployee.com>
2026-06-08 11:22:00 +0530

    CORS updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -51,7 +51,9 @@ app.add_middleware(SlowAPIMiddleware)
 ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:3000",
-    "https://ai-dam-six.vercel.app",
+    "ai-dam-six.vercel.app",
+    "ai-dam-git-main-sarthak-ve-s-projects.vercel.app",
+    "ai-idaigqetz-sarthak-ve-s-projects.vercel.app"
     # "http://127.0.0.1:5173"
 ]
  
